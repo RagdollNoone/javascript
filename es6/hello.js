3ea67d54f7598cfc342ac84880b0416d651595a7
dendy-company <ragdollnoone@163.com>
2023-11-07 15:19:25 +0800

    update

---
+++ es6/hello.js
@@ -0,0 +1,14 @@
+// 指定组件
+export const util = {
+    sum(a, b) {
+        return a + b;
+    }
+}
+
+// 默认组件
+export default {
+    sum(a, b) {
+        return a + b;
+    }
+}
+
---
+++ es6/user.js
@@ -0,0 +1,7 @@
+let name = "jack";
+let age = 21;
+function add(a, b) {
+    return a + b;
+}
+
+export {name, age, add}
